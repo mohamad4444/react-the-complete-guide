47ac8e81b4bd99c6003f64079f0d1b1740cef6d2
Mohammad Mahamid <mzkariyah@gmail.com>
2024-06-04 03:03:03 +0200

    initial

---
+++ Section 2/Javascript/assets/scripts/util.js
@@ -0,0 +1,8 @@
+//export let apiKey = "apishe";
+// export default "defaultvalue";
+//can't add two defult exports
+
+// export let apiKey = "apishe";
+// export let abc="abc";
+
+
---
+++ Section 2/Javascript/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html'); // Path to your index.html file
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Something went wrong');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+const PORT = 3000; // Choose a port number
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
